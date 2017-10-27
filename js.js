9edda94c8634728f99770c875689d20adafd6b0c
raygaratty232 <garatty.1411@gmail.com>
2017-10-27 20:07:32 +0300

    commit-2

--- js.js
+++ js.js
@@ -4,3 +4,37 @@ var sideToggle = document.getElementById('side-toggle')
 		var sideMenu = document.querySelector('.side-menu');
 		sideMenu.classList.toggle('active');
 	});
+
+	
+	$(document).ready(function(){
+    $(".fix-menu").removeClass("default");
+    $(window).scroll(function(){
+        if($(this).scrollTop() > 400){
+            $(".fix-menu").addClass("default").fadein('fast');
+        }
+        else{
+            $(".fix-menu").removeClass("default").fadein('fast')
+        }
+    })
+})
+
+$(".menu a").mPageScroll2id({
+    offset : 50
+});
+$(".slide-container a").mPageScroll2id({
+    offset : 40
+});
+
+
+$('.totop').tottTop({
+    duration: 400,
+    scrollTop: 400,
+    scrtollTopBtnDuration: 400
+});
+
+window.onload = function() {
+  Particles.init({
+    selector: '.background'
+  });
+}; 
+
